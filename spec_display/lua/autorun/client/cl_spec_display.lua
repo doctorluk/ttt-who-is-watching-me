1aa3f23b0690e94c19278bb9265d47a4d4ce385a
Lukas Monka <lukgit@gmail.com>
2017-05-21 11:53:38 +0200

    First working draft

--- spec_display/lua/autorun/client/cl_spec_display.lua
+++ spec_display/lua/autorun/client/cl_spec_display.lua
@@ -4,25 +4,42 @@
 
 if CLIENT then
 
+	-- Basic vars
+	local showEye = false
+	local showAmount = false
+	local amount = 0
 	
-	local ourMat = Material( "hud/spectator_eye.png" ) -- Calling Material() every frame is quite expensive
+	-- Receival of the server's spectator count
+	net.Receive( "spec_display", function( net_response )
+		amount = net.ReadInt( 8 )
+		showAmount = net.ReadBool( )
+		showEye = net.ReadBool( )
+	end)
+	
+	-- Eye-Icon on the side
+	local spec_eye = Material( "hud/spectator_eye.png" )
 
+	-- Drawing of the eye
 	hook.Add( "HUDPaint", "spectator_eye_image", function()
-		surface.SetDrawColor( 255, 255, 255, 255 )
-		surface.SetMaterial( ourMat	) -- If you use Material, cache it!
-		surface.DrawTexturedRect( 32, ScrH()/2-32, 64, 64 )
+		if amount > 0 and showEye then
+			surface.SetDrawColor( 255, 255, 255, 255 )
+			surface.SetMaterial( spec_eye )
+			surface.DrawTexturedRect( 32, ScrH()/2-32, 64, 64 )
+		end
 	end )
 	
+	-- Drawing of the amount of spectators
 	hook.Add( "HUDPaint", "spectator_eye_amount", function()
-		local zahl = 5
-		surface.SetFont( "Trebuchet24" )
-		surface.SetTextColor( 255, 255, 255, 255 )
-		if zahl < 10 then
-			surface.SetTextPos( 14, ScrH()/2-12 )
-		else
-			surface.SetTextPos( 6, ScrH()/2-12 )
+		if showAmount and amount > 0 then
+			surface.SetFont( "Trebuchet24" )
+			surface.SetTextColor( 255, 255, 255, 255 )
+			if amount < 10 then
+				surface.SetTextPos( 14, ScrH()/2-12 )
+			else
+				surface.SetTextPos( 6, ScrH()/2-12 )
+			end
+			surface.DrawText( amount )
 		end
-		surface.DrawText( zahl )
 	end )
 	
 end
--- spec_display/lua/autorun/server/sv_spec_display.lua
+++ spec_display/lua/autorun/server/sv_spec_display.lua
@@ -3,25 +3,77 @@
 
 if SERVER then
 	
+	resource.AddFile( "materials/hud/spectator_eye.png" )
+	
 	util.AddNetworkString( "spec_display" )
-	local currentlyObserved = {}
+	
+	local GROUP_ALIVE = 1
+	local GROUP_NOTFOUND = 2
+	local GROUP_FOUND = 3
+	local GROUP_SPEC = 4
+	
+	local showInnos = false
 	
 	local function checkSpectators( )
 		for _, ply in ipairs( player.GetHumans() ) do
-			if ply:IsSpec() then
+			ply.viewercount = 0
+		end
+		
+		for _, ply in ipairs( player.GetHumans() ) do
+			if ply:IsSpec() or not ply:Alive() then
 				local target = ply:GetObserverTarget()
-				if target and ply and ( ply:GetObserverMode() == OBS_MODE_IN_EYE or ply:GetObserverMode() == OBS_MODE_CHASE )then
-					currentlyObserved[#currentlyObserved] = {target, amount}
+				if target and ( ply:GetObserverMode() == OBS_MODE_IN_EYE or ply:GetObserverMode() == OBS_MODE_CHASE ) then
+					if not target.viewercount then target.viewercount = 0 end
+					target.viewercount = target.viewercount + 1
 				end
 			end
+		end
+		
+		showInnos = spec_canInnosSee()
+		
+		for _, ply in ipairs( player.GetHumans() ) do
+			net.Start( "spec_display" )
+			net.WriteInt( ply.viewercount, 8 )
+			net.WriteBool( ply:IsTraitor() )
+			net.WriteBool( showInnos )
+			net.Send( ply )
 		end
 	end
 	timer.Create( "spectate_monitor", 1, 0, checkSpectators )
 	
-	local function sendSpectatorWatching(ply, amount)
-		net.Start( "spec_display" )
-		net.WriteInt( amount, 8 )
-		net.Send(ply)
+	-- Check if innocents can see spectator eyes
+	function spec_canInnosSee( )
+		
+		for _, ply in ipairs( player.GetHumans() ) do
+			if getScoreGroup(ply) == GROUP_SPEC then
+				return true
+			end
+			if getScoreGroup(ply) == GROUP_FOUND  then
+				return true
+			end
+		end
+		
+		return false
+		
+	end
+	
+	function getScoreGroup(p)
+	   if not IsValid(p) then return -1 end -- will not match any group panel
+
+		if DetectiveMode() then
+			if p:IsSpec() and (not p:Alive()) then
+				if p:GetNWBool("body_found", false) then
+					return GROUP_FOUND
+				else
+					return GROUP_NOTFOUND
+				end
+			elseif p:Alive() and p:IsActive() then
+				return GROUP_ALIVE
+			end
+	   end
+
+	   return GROUP_SPEC
+	   
 	end
 	
 end
